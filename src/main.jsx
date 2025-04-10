48b8bd9c80e3d4738504afce1ae1ce903aad0c39
Daiana Palacios <daianapalacios213@gmail.com>
2025-04-10 11:53:07 -0300

    tercercommit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import App from './App.jsx'
+// src/main.jsx (Corrección)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'; // o import App from './App.jsx'; (Elige uno y sé consistente)
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
