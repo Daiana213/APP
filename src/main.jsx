af72bc672553b8648a4c69867d3f82654245e773
Daiana Palacios <daianapalacios213@gmail.com>
2025-04-10 18:55:36 -0300

    10commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx (Corrección)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'; // o import App from './App.jsx'; (Elige uno y sé consistente)
+import App from './App.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
